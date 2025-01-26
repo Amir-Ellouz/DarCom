d289c83058f434f4d5b3010cad27f4ef2220a8a0
Amir Ellouz <amirellouz.ae@gmail.com>
2025-01-26 20:45:12 +0100

    added contact us page

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css'],
+  standalone: false,
+})
+export class ContactComponent {
+  form: FormGroup;
+  constructor() {
+    this.form = new FormGroup({
+      email: new FormControl(),
+      name: new FormControl(),
+      message: new FormControl(),
+    });
+  }
+  onSubmit() {
+    console.log('submitted');
+    this.form.reset();
+  }
+}
